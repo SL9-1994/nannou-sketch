93e0752e55b351ff49992bfb76126548ce8efcaa
SL9-1994 <oktm1220@gmail.com>
2024-02-03 21:31:30 +0900

    add:✨ 必要な関数・構造体を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+// Nannouのアイテムを一括インポート。
+use nannou::prelude::*;
+
+/// アプリケーションのメインモデルを表します。
+struct Model {}
+
 fn main() {
- println!("Hello Nannou!")
+    nannou::app(model).event(event).simple_window(view).run();
 }
+
+/// モデルを初期化します。
+///
+/// # Arguments
+///
+/// * `_app` - `App` 構造体への参照。
+///
+/// # Returns
+///
+/// 初期化された `Model` 構造体。
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+/// イベントがトリガーされたときにモデルを更新します。(イベントハンドラ)
+///
+/// # Arguments
+///
+/// * `_app` - `App` 構造体への参照。
+/// * `_model` - `Model` 構造体への参照。
+/// * `_event` - トリガーされたイベント。
+fn event(_app: &App, _model: &mut Model, _event: Event) {}
+
+/// アプリケーションの描画処理を行います。
+///
+/// # Arguments
+///
+/// * `_app` - `App` 構造体への参照。
+/// * `_model` - `Model` 構造体への参照。
+/// * `_frame` - ビューをレンダリングするフレーム。
+fn view(_app: &App, _model: &Model, _frame: Frame) {}
