6b013107fcbea9002de7a46cbf4316d2f656263a
SL9-1994 <oktm1220@gmail.com>
2024-02-04 17:09:36 +0900

    add:✨ 正円アニメーションを作成

--- src/main.rs
+++ src/main.rs
@@ -42,10 +42,26 @@ fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
 
     // キャンバスの背景色を設定
-    draw.background().color(WHITE);
+    draw.background().color(GRAY);
 
     // 1辺が200の正方形を原点を中心として表示
-    draw.rect().x_y(0.0, 0.0).w_h(200.0, 200.0).color(ORANGE);
+    // draw.rect().x_y(0.0, 0.0).w_h(200.0, 200.0).color(ORANGE);
+
+    // Windowの幅を取得
+    let boundary = app.window_rect();
+
+    // 正弦波を作成
+    let sine = app.time.sin();
+
+    // 速度を1/2にした正弦波を作成
+    let slowersine = (app.time / 2.0).sin();
+
+    // 正弦波をWindowのx,yの最大値・最小値にマッピングする
+    let x = map_range(sine, -1.0, 1.0, boundary.left(), boundary.right());
+    let y = map_range(slowersine, -1.0, 1.0, boundary.bottom(), boundary.top());
+
+    // 正円を作成
+    draw.ellipse().w_h(x / 2.0, y / 2.0).color(WHITE).x_y(x, y);
 
     // フレームにレンダリング
     draw.to_frame(app, &frame).unwrap();
