f71441d47852c95a290082777f7cae0ec1a85b39
SL9-1994 <oktm1220@gmail.com>
2024-02-04 14:56:16 +0900

    add:✨ 正方形を作成

--- src/main.rs
+++ src/main.rs
@@ -37,4 +37,16 @@ fn event(_app: &App, _model: &mut Model, _event: Event) {}
 /// * `_app` - `App` 構造体への参照。
 /// * `_model` - `Model` 構造体への参照。
 /// * `_frame` - ビューをレンダリングするフレーム。
-fn view(_app: &App, _model: &Model, _frame: Frame) {}
+fn view(app: &App, _model: &Model, frame: Frame) {
+    // キャンバスを取得
+    let draw = app.draw();
+
+    // キャンバスの背景色を設定
+    draw.background().color(WHITE);
+
+    // 1辺が200の正方形を原点を中心として表示
+    draw.rect().x_y(0.0, 0.0).w_h(200.0, 200.0).color(ORANGE);
+
+    // フレームにレンダリング
+    draw.to_frame(app, &frame).unwrap();
+}
